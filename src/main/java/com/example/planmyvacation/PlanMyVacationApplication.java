fe0e5153cd2319925f9568a37147819cf3955c1f
krasih <something@softuni>
2024-07-11 15:06:51 +0300

    Initial commit

---
+++ src/main/java/com/example/planmyvacation/PlanMyVacationApplication.java
@@ -0,0 +1,13 @@
+package com.example.planmyvacation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanMyVacationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanMyVacationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/planmyvacation/PlanMyVacationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.planmyvacation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanMyVacationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
