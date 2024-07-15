36aa5a65fc0e35c91dda701f5a67c9c9a36f8619
krasih <something@softuni>
2024-07-15 09:59:06 +0300

    Adding more html templates. Improve Login and Register pages

--- src/main/java/com/example/planmyvacation/web/HomeController.java
+++ src/main/java/com/example/planmyvacation/web/HomeController.java
@@ -12,10 +12,34 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/index")
+    public String index() {
+
+        return "index";
+    }
+
     @GetMapping("/test")
     public String test() {
 
         return "test";
+    }
+
+    @GetMapping("/test2")
+    public String test2() {
+
+        return "test2";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+
+        return "register";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+
+        return "login";
     }
 
 }
