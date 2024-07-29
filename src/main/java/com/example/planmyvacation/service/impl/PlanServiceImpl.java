21c4e6fd8a8765d19d4794b0fcbadb3d21683f84
krasih <something@softuni>
2024-07-29 16:06:15 +0300

    Replace hardcoded user with the real logged user on plan creation (in PlanServiceImpl)

--- src/main/java/com/example/planmyvacation/service/impl/PlanServiceImpl.java
+++ src/main/java/com/example/planmyvacation/service/impl/PlanServiceImpl.java
@@ -15,6 +15,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -114,14 +117,18 @@ public class PlanServiceImpl implements PlanService {
                 .orElse(null);
     }
 
-    //    TODO: If need to use this method rises in more classes, consider adding it in some Utils class maybe...
     private User getCurrentUser() {
 
-        // TODO: Fill the USER once the Spring Security is implemented
-//        Optional<User> currentUser = userRepository.findById(userSession.id());
-//        if (currentUser.isEmpty()) return false;
-        // TODO: For now just hardcode user="user", (TO DELETE)
-        return userRepository.findByUsername("user").get();
+        User currentUser = null;
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            currentUser = userRepository.findByUsername(currentUserName).get();
+        }
+
+        return currentUser;
     }
 
     private Plan setPlan(PlanCreateDTO planDTO) {
